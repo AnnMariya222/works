6187dbc59f55918aaf07370eb6486ba88cc69402
AnnMariya222 <85779971+AnnMariya222@users.noreply.github.com>
2021-06-22 12:05:34 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,13 @@
+pi=3.14
+r=float(input("Enter the radius of the circle:"))
+area=pi*r*r
+print("The area of the circle of radius",r,"is:",area)
+
+
+
+
+file=input("Enter the filename")
+f=file.split(".")
+print("The extension of the file is:" + repr(f[-1]))
+      
+
---
+++ python4.py
@@ -0,0 +1,27 @@
+# Python program to print positive Numbers in a List 
+
+  
+# list of numbers 
+
+list1 = [12, -7, 5, 64, -14] 
+list2 = [12, 14, -95, 3]
+
+# iterating each number in list 1
+
+for num in list1: 
+
+      
+
+    # checking condition 
+
+    if num >= 0: 
+
+       print(num, end = " ")
+       
+print("\n")
+
+for number in list2:
+
+    if number >=0:
+
+        print(number, end =" ")
